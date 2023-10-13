a4032cb37626760189b76d32ed8664a1cff03b44
FarrukhSajjad46 <FarrukhSajjad46@gmail.com>
2023-10-13 19:06:15 +0500

    Master

---
+++ work.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("This is Simple TS Code");
+// Variables
+// String
+let fname = "Mr.FS";
+// Number
+let age = 22;
+// Boolean
+let smart = true;
+// Output
+console.log(fname, age, smart);
---
+++ work.ts
@@ -0,0 +1,10 @@
+console.log("This is Simple TS Code");
+// Variables
+// String
+let fname:string = "Mr.FS";
+// Number
+let age:number = 22;
+// Boolean
+let smart:boolean = true;
+// Output
+console.log(fname,age,smart);
